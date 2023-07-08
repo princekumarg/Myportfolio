ee5bd3f35b1f75b5689d323679d62eebd2b88da1
prince kumar <agarwal8789@gmail.com>
2023-07-09 02:17:00 +0530

    commit made by prince

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,47 @@
+const dynamicContent = document.getElementById("dynamic-text");
+console.log(dynamicContent)
+
+const phrases = ["Frontend Developer ...", "Machine Learning Engineer ...", "Deveops Tools Engineer...", "Open Source Contributor ..."]
+let pharseIndex = 0;
+let letterIndex = 0;
+const typingSpeed = 150;
+const erasingSpeed = 75
+
+function printLetters(phrase) {
+
+    if (letterIndex == phrase.length) {
+        // clear letter which have been typed
+        clearLetters();
+    }
+    else if (letterIndex < phrase.length) {
+        dynamicContent.textContent += phrase.charAt(letterIndex);
+        letterIndex += 1;
+        setTimeout(function () {
+            printLetters(phrase)
+        }, typingSpeed)
+    }
+}
+
+function clearLetters() {
+    if (letterIndex == -1) {
+        pharseIndex = (pharseIndex + 1) % phrases.length;
+        letterIndex = 0;
+        printLetters(phrases[pharseIndex])
+    }
+    else if (letterIndex > -1) {
+        let updatedPhrase = "";
+        for (let index = 0; index < letterIndex; index++) {
+            updatedPhrase += phrases[pharseIndex].charAt(index);
+
+        }
+        console.log(updatedPhrase);
+        dynamicContent.textContent = updatedPhrase;
+        letterIndex -= 1;
+        setTimeout(clearLetters, erasingSpeed)
+    }
+}
+
+printLetters(phrases[pharseIndex])
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
